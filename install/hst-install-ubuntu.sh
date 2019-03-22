132540e4301391e58bf9e7ed8d018beaf1369145
Raphael Schneeberger <rs@scit.ch>
2019-03-22 10:08:17 +0100

    Fix possible language issues for add-apt-repository.

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -539,7 +539,7 @@ APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add /tmp/nginx_signing.key > /dev
 
 # Installing sury php repo
 echo "(*) PHP"
-add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php > /dev/null 2>&1
 
 # Installing z-push repo
 if [ "$exim" == 'yes' ]; then
